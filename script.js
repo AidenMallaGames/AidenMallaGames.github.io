7991a2380d81841dc6a85bdbd8c0070bc2e81070
AidenMallaGames <81830696+AidenMallaGames@users.noreply.github.com>
2024-02-06 12:58:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var url = prompt("Enter URL Here (example: https://google.com)");if (url == false) {alert('No URL Entered!');} else {document.body.innerHTML = '<iframe src="https://' + url +'" style="position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;">Your browser does not support iframes</iframe>';alert('URL Loaded!');}
+// script.js
+
+function loadUrl() {
+    var url = prompt("Enter URL Here (example: https://google.com)");
+
+    if (url === null) {
+        alert('No URL Entered!');
+        return;
+    }
+
+    // Add default protocol if missing
+    if (!/^https?:\/\//i.test(url)) {
+        url = 'https://' + url;
+    }
+
+    var iframe = document.createElement('iframe');
+    iframe.src = url;
+    iframe.style.cssText = 'position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;z-index:99;';
+    iframe.textContent = 'Your browser does not support iframes';
+    
+    // Replace the body content with the iframe
+    document.body.innerHTML = '';
+    document.body.appendChild(iframe);
+    
+    alert('URL Loaded!');
+}
+
+loadUrl();
